e1ccab7f7eacc8812228fa49fdbb9d1d79dd6e59
cdheij <Chris.Heij@logius.nl>
2023-08-30 11:48:51 +0200

    setup card story

---
+++ packages/storybook/src/logius/card.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react';
+// import { UtrechtButton, UtrechtIcon } from '@utrecht/web-component-library-react';
+// import { PropsWithChildren } from 'react';
+import readme from './card.md?raw';
+
+// interface CardStoryProps {
+//
+// }
+
+const CardStory = () => (
+  <div>
+    <h2>Hello world card</h2>
+  </div>
+);
+
+const meta = {
+  title: 'Logius/Card',
+  id: 'logius-card',
+  component: CardStory,
+  argTypes: {
+    children: {
+      description: 'Card text - default webcomponent slot',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      table: {
+        category: 'Webcomponent Slot',
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+  render: CardStory,
+} as Meta<typeof CardStory>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    children: 'Label',
+  },
+};
