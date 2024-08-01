a71b1d9f836bc569b98b64cf4a256a466e7c46f9
Ruben Smit <rsmit@quintor.local>
2024-08-01 10:51:43 +0200

    chore: added exports to index.ts

--- packages/components-react/src/index.ts
+++ packages/components-react/src/index.ts
@@ -1,3 +1,6 @@
+export * from '@utrecht/component-library-react/dist/css-module';
+
+// Overwrites for Utrecht components if they have the same name.
 export { Button } from './Button';
 export { Logo } from './Logo';
 export type { LogoProps } from './Logo';
